803562cc805b3a8fe70ff599569118c467fe608b
vladimir blanco <vladimirblanco@datatraffic.com>
2020-09-14 21:15:50 -0400

    init

---
+++ js/script.js
@@ -0,0 +1,10 @@
+(function($) {
+  var typed = new Typed('span.txt-rotate', {
+    strings: ['I\'m an Angular Developer.', 'I\'m also a VueJS Developer.', 'And I\'m a NodeJS Developer.', 'Then I\'m a Fullstack Javascript Developer.'],
+    typeSpeed: 50,
+    backSpeed: 20,
+    fadeOut: false,
+    smartBackspace: true,
+    loop: true
+  })
+})(jQuery);
